9a4bf48a39085fbdcbf075a7f81a048f9f5c2f3d
Silvio Silva <smsilva@gmail.com>
2023-11-15 18:07:39 -0300

    feat: Initial setup

---
+++ src/test/java/com/github/smsilva/batch/demo/SpringBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.smsilva.batch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
