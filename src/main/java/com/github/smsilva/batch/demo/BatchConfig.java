4133835e21c0106cdddd2cf1b44a273ef390fb93
Silvio Silva <smsilva@gmail.com>
2023-11-15 18:36:47 -0300

    feat: Hello World Job

---
+++ src/main/java/com/github/smsilva/batch/demo/BatchConfig.java
@@ -0,0 +1,33 @@
+package com.github.smsilva.batch.demo;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Component
+public class BatchConfig {
+
+    @Bean
+    public Job job(JobRepository repository, Step step) {
+        return new JobBuilder("demoJob", repository)
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step step(JobRepository repository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("demoStep", repository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Hello, World!");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+}
--- src/test/java/com/github/smsilva/batch/demo/SpringBatchApplicationTests.java
+++ src/test/java/com/github/smsilva/batch/demo/SpringBatchApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBatchDemoApplicationTests {
+class SpringBatchApplicationTests {
 
 	@Test
 	void contextLoads() {
