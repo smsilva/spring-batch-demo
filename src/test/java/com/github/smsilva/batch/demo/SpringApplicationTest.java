be75517dba177493682fb50ed2a7bc9cc5f55627
Silvio Silva <smsilva@gmail.com>
2023-11-16 20:56:50 -0300

    feat: Integration test

--- src/test/java/com/github/smsilva/batch/demo/SpringApplicationTest.java
+++ src/test/java/com/github/smsilva/batch/demo/SpringApplicationTest.java
@@ -1,13 +1,44 @@
 package com.github.smsilva.batch.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@SpringBatchTest
 class SpringApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    public void clearingDatabase(DataSource dataSource) {
+        System.out.println("Clearing database");
+
+        JdbcTemplate template = new JdbcTemplate(dataSource);
+        template.update("DELETE FROM customers");
+
+        for (int i = 1; i <= 10; i++) {
+            template.update("INSERT INTO customers(id, name) VALUES (?, ?)", i, "customer" + i);
+        }
+    }
+
+    @Test
+    public void testJob(@Autowired Job demoJob) throws Exception {
+        this.jobLauncherTestUtils.setJob(demoJob);
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+        assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
+    }
 
 }
--- src/test/resources/db/customers-insert.sql
+++ src/test/resources/db/customers-insert.sql
@@ -1,13 +1,11 @@
 CREATE TABLE IF NOT EXISTS customers (
-  id         BIGSERIAL NOT NULL,
-  first_name VARCHAR(50) NOT NULL,
-  last_name  VARCHAR(50) NOT NULL,
-  name       VARCHAR(100) NOT NULL,
-  email      VARCHAR(100) NOT NULL,
+  id    BIGSERIAL    NOT NULL,
+  name  VARCHAR(100) NOT NULL,
+  email VARCHAR(100),
   PRIMARY KEY (id),
   UNIQUE (email)
 );
 
-INSERT INTO customers(id, first_name, last_name, name, email) VALUES (1, 'John', 'Doe', 'John Doe', 'john.doe@example.com');
-INSERT INTO customers(id, first_name, last_name, name, email) VALUES (2, 'Jane', 'Doe', 'Jane Doe', 'jane.doe@example.com');
-INSERT INTO customers(id, first_name, last_name, name, email) VALUES (3, 'Richard', 'Doe', 'Richard Doe', 'richard.doe@example.com');
+INSERT INTO customers(id, name, email) VALUES (1, 'John Doe'   , 'john.doe@example.com'   );
+INSERT INTO customers(id, name, email) VALUES (2, 'Jane Doe'   , 'jane.doe@example.com'   );
+INSERT INTO customers(id, name, email) VALUES (3, 'Richard Doe', 'richard.doe@example.com');
